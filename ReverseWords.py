c6062b10a4a806a0e421b9d8aea52d61d7263d94
Brian McIlwain <bmcilw1@gmail.com>
2016-01-22 11:15:48 -0600

    Usable via stdout and piping

--- ReverseWords.py
+++ ReverseWords.py
@@ -1,4 +1,4 @@
-#!/usr/bin python
+#!/usr/bin/python
 
 from __future__ import print_function, absolute_import
 import sys
@@ -6,20 +6,18 @@ import sys
 def main():
     filename = sys.argv[1]
     f = open(filename, 'rU')
-    out = open("out.txt", 'w')
     cases = int(f.readline())
 
     for case in range(1, cases + 1):
         wordList = f.readline().split()
 
-        print("Case #" + str(case) + ":", file=out, end="")
+        print("Case #" + str(case) + ":", end="")
 
         for word in reversed(wordList):
-            print(" " + word, file=out, end="")
+            print(" " + word, end="")
         
-        print("", file=out)
+        print("")
 
-    out.close()
     f.close()
 
 # Standard boilerplate to call the main() function to begin
